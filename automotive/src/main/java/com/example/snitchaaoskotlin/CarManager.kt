859a86e36f9c724b3beafc52d903420886b976f1
Alon Ronder <alonr110@gmail.com>
2023-02-26 19:32:42 +0200

    Upload basic project

---
+++ automotive/src/main/java/com/example/snitchaaoskotlin/CarManager.kt
@@ -0,0 +1,15 @@
+package com.example.snitchaaoskotlin
+
+import androidx.car.app.CarAppService
+import androidx.car.app.Session
+import androidx.car.app.validation.HostValidator
+
+class CarManager : CarAppService() {
+    override fun createHostValidator(): HostValidator {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCreateSession(): Session {
+        TODO("Not yet implemented")
+    }
+}
